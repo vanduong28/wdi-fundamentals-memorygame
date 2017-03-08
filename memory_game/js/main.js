869045eb1c6fd7f5c5a180c8484e2264e1728dd0
Van Duong <vanduong@Vans-MacBook-Pro.local>
2017-03-08 09:14:30 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
 var cardOne  = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardThree); */
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped queen");
+console.log("User flipped king");
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+}
