3eecf7fc1508dd19725bc4340bdbacacab104c91
Van Duong <vanduong@Vans-MacBook-Pro.local>
2017-03-13 14:03:24 -0700

    unit 11 assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,8 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var gameBoard = document.getElementById('game-board');
+
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -30,15 +32,32 @@ var checkForMatch = function(){
 		alert("Sorry, try again");
 	}
 }
-var flipCard = function(cardId){
+
+var isTwoCards = function(){
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+}
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	isTwoCards();
 	
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		gameBoard.appendChild(cardElement);
 
+	}
+}
+
+createBoard();
